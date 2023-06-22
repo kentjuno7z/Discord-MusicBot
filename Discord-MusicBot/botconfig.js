f641bd1a3f55cb5eefa5e9ee1df8fb25d97b3f65
Lamdev <96229800+kentjuno7z@users.noreply.github.com>
2023-06-22 18:55:03 +0700

    Update botconfig.js

--- Discord-MusicBot/botconfig.js
+++ Discord-MusicBot/botconfig.js
@@ -14,7 +14,7 @@ module.exports = {
   "24/7": false, // Make the bot stays in VC 24/7 (when you reboot the bot will **not** automatically rejoin.)
   CookieSecret: "Pikachu is cute", // A cookie for you, cookie for me. make sure you change this value!
   IconURL:
-    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", // URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+    "/", // URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", // Color of most embeds | Custom Hex value are supported. I.e: "#36393F"
   Permissions: 2205281600, // Bot Inviting Permissions
   Website: process.env.Website || "https://example.com", // Website where it is hosted at includes http or https || Use "0.0.0.0" if you using Heroku || Do not include /api/callback. Just the website url. I.e. "https://foo.bar"
